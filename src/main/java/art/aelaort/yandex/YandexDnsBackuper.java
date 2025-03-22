19ecf4f5c26763ed78b3f70cbabb60463d1bd347
ort <orthodoxplus@ya.ru>
2025-03-22 18:52:55 +0300

    ApplicationRunner

--- src/main/java/art/aelaort/yandex/YandexDnsBackuper.java
+++ src/main/java/art/aelaort/yandex/YandexDnsBackuper.java
@@ -5,7 +5,8 @@ import art.aelaort.yandex.dto.ZoneInfo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
@@ -16,13 +17,12 @@ import static art.aelaort.Utils.now;
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class YandexDnsBackuper {
+public class YandexDnsBackuper implements ApplicationRunner {
 	private final YandexDnsService yandexDnsService;
 	private final S3Params yandexDnsS3;
 	@Value("${yandex.dns.s3.bucket}")
 	private String bucket;
 
-	@Scheduled(cron = "${cron.dns.backup}")
 	public void backupDns() {
 		String now = now();
 		for (ZoneInfo zone : yandexDnsService.getZones()) {
@@ -33,7 +33,7 @@ public class YandexDnsBackuper {
 		}
 	}
 
-	public void uploadFileContent(String fileId, String fileContent) {
+	private void uploadFileContent(String fileId, String fileContent) {
 		try (S3Client client = client(yandexDnsS3)) {
 			client.putObject(builder -> builder
 							.key(fileId)
@@ -41,5 +41,10 @@ public class YandexDnsBackuper {
 							.build(),
 					RequestBody.fromString(fileContent));
 		}
+	}
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		backupDns();
 	}
 }
